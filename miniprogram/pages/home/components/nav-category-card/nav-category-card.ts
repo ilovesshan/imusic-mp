00c92a9c681b7f9e258539d570ee0bcd702548d8
wangyifeng <2665939276@qq.com>
2022-11-20 20:53:23 +0800

    feat: 完成分类导航和流行音乐家模块静态界面搭建
    feat: 完成分类导航和流行音乐家模块静态界面搭建

---
+++ miniprogram/pages/home/components/nav-category-card/nav-category-card.ts
@@ -0,0 +1,25 @@
+// pages/home/components/nav-category-card.ts
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    iconPath: { type: String },
+    title: { type: String },
+    paycount: { type: Number },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
